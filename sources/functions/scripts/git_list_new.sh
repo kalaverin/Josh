dd3890a6267961c77c5f44842a8751b39cb57aa0
Yaakov Tooth <tooth@yaakov.ru>
2020-10-22 09:22:50 +0300

    master: add new/deleted temporary fix

--- sources/functions/scripts/git_list_new.sh
+++ sources/functions/scripts/git_list_new.sh
@@ -1,1 +1,2 @@
-git status --porcelain --ignore-submodules --no-ahead-behind --untracked-files | awk 'match($1, "^\\?\\?$"){print $2}' | awk '{$1=$1};1'
+# git status --porcelain --ignore-submodules --no-ahead-behind --untracked-files | awk 'match($1, "^\\?\\?$"){print $2}' | awk '{$1=$1};1'
+git ls-files --deleted --others --exclude-standard `git rev-parse --show-toplevel`
