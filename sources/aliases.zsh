b1269dd67bdcbb1ddaa29d59a3cd7fea5ac781f1
Yaakov Tooth <tooth@yaakov.ru>
2021-01-27 01:13:23 +0300

    master: ar&rg options

--- sources/aliases.zsh
+++ sources/aliases.zsh
@@ -7,9 +7,11 @@ alias rm='rm'
 alias ps='ps'
 alias tt='tail -f -n 1000'
 
-alias ag='ag --noaffinity --ignore .git/ --ignore node_modules/ --ignore "lib/python*/site-packages/" --ignore "__snapshots__/" --ignore "*.pyc" --ignore "*.po" --ignore "*.svg" --literal --stats -W 140'
+alias ag='ag -C1 --noaffinity --path-to-ignore ~/.ignore --stats --smart-case --width 140'
 
-alias rg='rg --smart-case --context 1 --color always --require-git --stats --text --ignore-file ~/.ripgrep.conf --type-not css --type-not js --type-not json --sort path'
+alias rg='rg --smart-case --context 1 --context-separator "" --require-git --stats --text --ignore-file ~/.ignore --max-columns 140 --max-columns-preview --max-filesize 1M --color always --colors "match:fg:yellow" --colors "path:fg:red"'
+
+alias ri="$JOSH_GREP -rnH --exclude '*.js' --exclude '*.min.css' --exclude '.git/' --exclude 'node_modules/' --exclude 'lib/python*/site-packages/' --exclude '__snapshots__/' --exclude '.eggs/' --exclude '*.pyc' --exclude '*.po' --exclude '*.svg' --color=auto"
 
 alias -g I='| grep -i'
 alias -g E='| grep -iv'
@@ -20,7 +22,6 @@ alias -g NUL="> /dev/null 2>&1"
 alias -g GL="awk '{\$1=\$1};1' | sed -z 's/\n/ /g' | awk '{\$1=\$1};1'"
 
 alias ls="$JOSH_LS -vaAF --color"
-alias ri="$JOSH_GREP -rnH --exclude '*.js' --exclude '*.min.css' --exclude '.git/' --exclude 'node_modules/' --exclude 'lib/python*/site-packages/' --exclude '__snapshots__/' --exclude '.eggs/' --exclude '*.pyc' --exclude '*.po' --exclude '*.svg' --color=auto"
 
 alias sed="$JOSH_SED"
 alias grep="$JOSH_GREP"
