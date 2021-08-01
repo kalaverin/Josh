a68b0b22f526eec2a836fe89e3663ca19c631c19
Yaakov Tooth <tooth@yaakov.ru>
2021-08-01 22:59:46 +0300

    master: just fix mistake

--- install/units/configs.sh
+++ install/units/configs.sh
@@ -50,7 +50,7 @@ function config_git() {
     git config --global color.showbranch auto
     git config --global core.pager "delta --commit-style='yellow ul' --commit-decoration-style='' --file-style='cyan ul' --file-decoration-style='' --hunk-style normal --zero-style='dim syntax' --24-bit-color='always' --minus-style='syntax #330000' --plus-style='syntax #002200' --file-modified-label='M' --file-removed-label='D' --file-added-label='A' --file-renamed-label='R' --line-numbers-left-format='{nm:^4}' --line-numbers-minus-style='#aa2222' --line-numbers-zero-style='#505055' --line-numbers-plus-style='#229922' --line-numbers --navigate"
     return 0
-fi
+}
 
 
 function nano_syntax() {
