a1a5cd1d333cabae564de2d6ef291215efa468c0
Yaakov Tooth <tooth@yaakov.ru>
2021-08-01 23:49:26 +0300

    master: compat and delta new params

--- sources/compat.zsh
+++ sources/compat.zsh
@@ -7,20 +7,16 @@ if [[ "$OSTYPE" == "freebsd"* ]]; then
     export JOSH_REALPATH='/usr/local/bin/grealpath'
 
     export OS_TYPE="BSD"
-    export DELTA="delta --commit-style plain --file-style plain --hunk-style plain --highlight-removed"
 
 else
-    export JOSH_LS=`which ls`
-    export JOSH_SED=`which sed`
-    export JOSH_GREP=`which grep`
-    export JOSH_REALPATH=`which realpath`
+    export JOSH_LS=`which -p ls`
+    export JOSH_SED=`which -p sed`
+    export JOSH_GREP=`which -p grep`
+    export JOSH_REALPATH=`which -p realpath`
 
     export OS_TYPE="LINUX"
-    export DELTA="delta --commit-style='yellow ul' --commit-decoration-style='' --file-style='cyan ul' --file-decoration-style='' --hunk-style normal --zero-style='dim syntax' --24-bit-color='always' --minus-style='syntax #330000' --plus-style='syntax #002200' --file-modified-label='M' --file-removed-label='D' --file-added-label='A' --file-renamed-label='R' --line-numbers-left-format='{nm:^4}' --line-numbers-minus-style='#aa2222' --line-numbers-zero-style='#505055' --line-numbers-plus-style='#229922' --line-numbers --navigate"
 fi
 
-if [ -n "$(uname -v | grep -i debian)" ]; then
-    alias fd='fdfind'
-fi
+export DELTA="delta --commit-style='yellow ul' --commit-decoration-style='' --file-style='cyan ul' --file-decoration-style='' --hunk-style normal --zero-style='dim syntax' --24-bit-color='always' --minus-style='syntax #330000' --plus-style='syntax #002200' --file-modified-label='M' --file-removed-label='D' --file-added-label='A' --file-renamed-label='R' --line-numbers-left-format='{nm:^4}' --line-numbers-minus-style='#aa2222' --line-numbers-zero-style='#505055' --line-numbers-plus-style='#229922' --line-numbers --navigate --relative-paths --hyperlinks"
 
 export JOSH_HTTP=`which -p http`
