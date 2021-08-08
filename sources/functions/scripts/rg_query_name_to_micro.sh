7c067fb011de6fcaa4f064550e821da64010d414
Yaakov Tooth <tooth@yaakov.ru>
2021-08-08 23:48:03 +0300

    master: rg previewer debug

--- sources/functions/scripts/rg_query_name_to_micro.sh
+++ sources/functions/scripts/rg_query_name_to_micro.sh
@@ -1,1 +1,9 @@
-sd '(.+?):(.+?):\d' '"$1" $2' | xargs -I% echo rg --smart-case --no-heading --column --with-filename --max-count=1 --color=never "%" | $SHELL | tabulate -d ':' -i 1,2,3 | sd '(.+?)\s+(\d+)\s+(\d+)' 'micro -mouse=false $1 +$2:$3'
+# local command=$(sd '(.+?):(.+?)$' '"$1" $2' | xargs $JOSH_RIPGREP --smart-case --fixed-strings --no-heading --column --with-filename --max-count=1 --color=never | tabulate -d ':' -i 1,2,3 | sd '(.+?)\s+(\d+)\s+(\d+)' '$1:$2:$3')
+# local position=$(echo "$command" | grep -Po '(\d+:\d+)$')
+# local filename=$(echo "$command" | sd '(:\d+:\d+)$' '')
+# micro $filename $position
+
+# local command=$(sd '(.+?):(.+?)$' '"$1" $2' | xargs $JOSH_RIPGREP --smart-case --fixed-strings --no-heading --column --with-filename --max-count=1 --color=never | tabulate -d ':' -i 1,2,3 | sd '(.+?)\s+(\d+)\s+(\d+)' '$1 +$2:$3')
+# echo $command | tabulate | xargs micro
+
+sd '(.+?):(.+?)$' '"$1" $2' | xargs $JOSH_RIPGREP --smart-case --fixed-strings --no-heading --column --with-filename --max-count=1 --color=never | tabulate -d ':' -i 1,2,3 | sd '(.+?)\s+(\d+)\s+(\d+)' 'micro $1 +$2:$3' | $SHELL
