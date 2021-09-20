212de3fe742556abbad595ba70cf4d3966bdeb00
Yaakov Tooth <tooth@yaakov.ru>
2021-09-20 18:13:22 +0300

    FreeBSD: and more and more verbose

--- run/boot.sh
+++ run/boot.sh
@@ -16,17 +16,19 @@ echo " + initial deploy to $DEST"
 git clone --depth 1 https://github.com/YaakovTooth/Josh.git $DEST
 [ $? -gt 0 ] && return 2
 
-if [ "$JOSH_BRANCH" ]; then
-    echo " + using Josh branch \`$JOSH_BRANCH\`"
+cd "$DEST/run/"
+[ $? -gt 0 ] && return 3
 
-    GIT="git --git-dir=\"$DEST/.git\" --work-tree=\"$DEST\""
-    local cmd="$GIT fetch origin "$JOSH_BRANCH":"$JOSH_BRANCH" && $GIT checkout --force --quiet $JOSH_BRANCH && $GIT reset --hard $JOSH_BRANCH && $GIT pull --ff-only --no-edit --no-commit --verbose origin $JOSH_BRANCH"
+if [ "$JOSH_BRANCH" ]; then
+    echo " + fetch Josh branch \`$JOSH_BRANCH\`"
+
+    local cmd="git fetch origin "$JOSH_BRANCH":"$JOSH_BRANCH" && git checkout --force --quiet $JOSH_BRANCH && git reset --hard $JOSH_BRANCH && git pull --ff-only --no-edit --no-commit --verbose origin $JOSH_BRANCH"
 
     echo " -> $cmd" && $SHELL -c "$cmd"
-    [ $? -gt 0 ] && return 3
+    [ $? -gt 0 ] && return 4
 fi
+echo " + using Josh branch \``git rev-parse --quiet --abbrev-ref HEAD`\`"
 
-cd "$DEST/run/"
 . "$DEST/run/strap.sh"
 
 check_requirements && \
