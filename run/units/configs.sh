852087d158c277305a7e668e7801d4aca6c201be
Yaakov Tooth <tooth@yaakov.ru>
2021-09-20 20:27:26 +0300

    FreeBSD: rewrite configs copier

--- run/units/configs.sh
+++ run/units/configs.sh
@@ -31,6 +31,36 @@ if [ ! "$CONFIG_DIR" ]; then
     fi
 fi
 
+function backup_file() {
+    if [ -f "$1" ]; then
+        local dst="$1+`date "+%Y%m%d%H%M%S"`"
+        if [ ! -f "$dst" ]; then
+            echo " + backup: $1 -> $dst" && cp -fa "$1" "$dst"
+        fi
+        return $?
+    fi
+}
+
+function copy_config() {
+    local dst="$2"
+    if [ -f "$dst" ] && [ ! "$JOSH_RENEW_CONFIGS" ]; then
+        echo " * already exists: \`$dst\`"
+        return 0
+    fi
+
+    local src="$1"
+    if [ ! -f "$src" ]; then
+        echo " - error: copy config failed, source \`$src\` not exists"
+        return 1
+    fi
+
+    [ -f "$dst" ] && backup_file "$dst" && unlink "$dst"
+    [ ! -d "`dirname $dst`" ] && mkdir -p "`dirname $dst`";
+
+    echo " + ${3:-"copy: $src -> $dst"}" && cp -n "$src" "$dst"
+    return $?
+}
+
 function copy_cargo() {
     local dst="$REAL/.cargo/config.toml"
     [ ! -d "`dirname $dst`" ] && mkdir -p "`dirname $dst`"; [ -f "$dst" ] && return 0
@@ -96,50 +126,62 @@ function copy_pgcli() {
 }
 
 function config_git() {
-    copy_gitignore
+    copy_config "$SOURCE_ROOT/usr/share/.gitignore" "$REAL/.gitignore"
+
     if [ ! "`git config --global core.pager | grep -P '^(delta)'`" ]; then
-        git config --global color.branch auto
-        git config --global color.decorate auto
-        git config --global color.diff auto
-        git config --global color.grep auto
-        git config --global color.interactive auto
-        git config --global color.pager true
-        git config --global color.showbranch auto
-        git config --global color.status auto
-        git config --global color.ui auto
+        backup_file "$REAL/.gitconfig" && \
+        git config --global color.branch auto && \
+        git config --global color.decorate auto && \
+        git config --global color.diff auto && \
+        git config --global color.grep auto && \
+        git config --global color.interactive auto && \
+        git config --global color.pager true && \
+        git config --global color.showbranch auto && \
+        git config --global color.status auto && \
+        git config --global color.ui auto && \
         git config --global core.pager "delta --commit-style='yellow ul' --commit-decoration-style='' --file-style='cyan ul' --file-decoration-style='' --hunk-style normal --zero-style='dim syntax' --24-bit-color='always' --minus-style='syntax #330000' --plus-style='syntax #002200' --file-modified-label='M' --file-removed-label='D' --file-added-label='A' --file-renamed-label='R' --line-numbers-left-format='{nm:^4}' --line-numbers-minus-style='#aa2222' --line-numbers-zero-style='#505055' --line-numbers-plus-style='#229922' --line-numbers --navigate"
         # git config --global core.filemode false
         # git config --global core.safecrlf true
         # git config --global core.autocrlf true
     fi
+
+    if [ ! "`git config --global sequence.editor`" ] && [ -f "`which -p interactive-rebase-tool`" ]; then
+        backup_file "$REAL/.gitconfig" && \
+        git config --global sequence.editor interactive-rebase-tool
+    fi
+
     return 0
 }
 
 function nano_syntax_compile() {
     if [ ! -f "$REAL/.nanorc" ]; then
         # https://github.com/scopatz/nanorc
-        if [ -d /usr/local/share/nano/ ]; then
-            find /usr/local/share/nano/ -iname "*.nanorc" -exec echo include {} \; >> $REAL/.nanorc
-        fi
+
         if [ -d /usr/share/nano/ ]; then
+            # linux
             find /usr/share/nano/ -iname "*.nanorc" -exec echo include {} \; >> $REAL/.nanorc
+
+        elif [ -d /usr/local/share/nano/ ]; then
+            # freebsd
+            find /usr/local/share/nano/ -iname "*.nanorc" -exec echo include {} \; >> $REAL/.nanorc
+
         fi
-        if [ -f "$REAL/.nanorc" ]; then
-            echo " + nano config $REAL/.nanorc generated"
-        fi
+        [ -f "$REAL/.nanorc" ] && echo " + nano syntax highlighting generated to $REAL/.nanorc ok"
     fi
     return 0
 }
 
 function zero_configuration() {
-    copy_cargo
-    copy_gitignore
-    copy_micro
-    copy_nodeenv
-    copy_pip
-    copy_starship
-    copy_mycli
-    copy_pgcli
+    local CONFIG_ROOT="$SOURCE_ROOT/usr/share"
+
     config_git
     nano_syntax_compile
+
+    copy_config "$CONFIG_ROOT/cargo.toml" "$REAL/.cargo/config.toml"
+    copy_config "$CONFIG_ROOT/micro.json" "$CONFIG_DIR/micro/settings.json"
+    copy_config "$CONFIG_ROOT/nodeenv.conf" "$REAL/.nodeenvrc"
+    copy_config "$CONFIG_ROOT/pip.conf" "$CONFIG_DIR/pip/pip.conf"
+    copy_config "$CONFIG_ROOT/starship.toml" "$CONFIG_DIR/starship.toml"
+    copy_config "$CONFIG_ROOT/mycli.conf" "$REAL/.myclirc"
+    copy_config "$CONFIG_ROOT/pgcli.conf" "$CONFIG_DIR/pgcli/config"
 }
