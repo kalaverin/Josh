51af615c4f07fc372ab65189079f04e36e0beb4a
Yaakov Tooth <tooth@yaakov.ru>
2021-09-24 13:54:56 +0300

    develop: conflict solver fix

--- usr/units/git.zsh
+++ usr/units/git.zsh
@@ -449,7 +449,7 @@ git_widget_conflict_solver() {
 
     local conflicted='xargs rg \
         --files-with-matches --with-filename --color always \
-        --count --heading --line-number "^(<<<<<<< HEAD)\s*$" | tabulate -d ":"'
+        --count --heading --line-number "^(<<<<<<< )" | tabulate -d ":"'
 
     local need_quit=0
     local last_hash=""
@@ -505,13 +505,12 @@ git_widget_conflict_solver() {
     zle reset-prompt
     if [ "$state" -eq 3 ]; then
         local state="`get_repository_state`"
-        local conflicted="$($SHELL -c "$select | $conflicted | $UNIQUE_SORT | wc -l")"
-        if [ "$conflicted" -gt 0 ]; then
-            local conflicted=" and $conflicted files with conflicts block auto$state"
+        local conflicts_amount="$($SHELL -c "$select | $conflicts_amount | $UNIQUE_SORT | wc -l")"
+        if [ "$conflicts_amount" -gt 0 ]; then
+            local conflicts_amount=" and $conflicts_amount files with conflicts block auto$state"
         else
-            local conflicted=", but no conflicts found (check git-add widget?)"
+            local conflicts_amount=", but no conflicts found (check git-add widget?)"
         fi
-        echo "* repository in state \`$state\`$conflicted\n"
     fi
     zle redisplay
     return 0
