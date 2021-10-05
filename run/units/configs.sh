48c3da46f493e03a1b6b8b3c07d184fef3dea13f
Yaakov Tooth <tooth@yaakov.ru>
2021-10-06 01:51:08 +0300

    feature/tmux: add tmux ad required

--- run/units/configs.sh
+++ run/units/configs.sh
@@ -114,4 +114,5 @@ function zero_configuration() {
     copy_config "$CONFIG_ROOT/mycli.conf" "$HOME/.myclirc"
     copy_config "$CONFIG_ROOT/pgcli.conf" "$CONFIG_DIR/pgcli/config"
     copy_config "$CONFIG_ROOT/ondir.rc" "$HOME/.ondirrc"
+    copy_config "$CONFIG_ROOT/tmux.conf" "$HOME/.tmux.conf"
 }
