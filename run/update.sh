7e0fa976128d541fd269b08dd19db488338f8374
Yaakov Tooth <tooth@yaakov.ru>
2021-10-07 00:11:46 +0300

    develop: fetch tags with josh_pull

--- run/update.sh
+++ run/update.sh
@@ -37,6 +37,7 @@ function pull_update() {
             echo " + pull last changes from \`$branch\` to \``pwd`\`" && \
             git pull --ff-only --no-edit --no-commit origin "$branch"
             local retval="$?"
+            git fetch --tags
 
             if [ $retval -eq 0 ]; then
                 git update-index --refresh &>>/dev/null
