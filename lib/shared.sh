a794ccfd0a022e1afed3a2fdd4b99bc3c1c237e6
Alex Kalaverin <alex@kalaver.in>
2022-06-01 16:58:06 +0300

    develop: last_modified_directory

--- lib/shared.sh
+++ lib/shared.sh
@@ -7,7 +7,7 @@ local UNIQUE_SORT="runiq - | proximity-sort ."
 local LINES_TO_LINE="sd '\n' ' ' | awk '{\$1=\$1};1'"
 
 
-function cpu_cores_count() {
+function cpu_cores_count {
     if [ "$JOSH_OS" = 'BSD' ]; then
         local cores="`sysctl kern.smp.cores | grep -Po '\d$'`"
     else
@@ -21,7 +21,7 @@ function cpu_cores_count() {
     fi
 }
 
-function get_preview_width() {
+function get_preview_width {
     let width="$COLUMNS - ($COLUMNS / 3 + 10)"
     [ $width -lt 84 ] && local width=84
 
@@ -29,50 +29,53 @@ function get_preview_width() {
     export JOSH_WIDTH="$width"
 }
 
-function get_tempdir() {
+function get_tempdir {
     local result="$(fs_dirname `mktemp -duq`)"
     [ ! -x "$result" ] && mkdir -p "$result"
     echo "$result"
 }
 
-
-function mkcd() {
+function mkcd {
     [ -z "$1" ] && return 1
     mkdir -p "$*" && cd "$*"
 }
 
-
-function run_show() {
+function run_show {
     local cmd="$*"
     [ -z "$cmd" ] && return 1
     echo " -> $cmd" 1>&2
     eval ${cmd} 1>&2
 }
 
-
-function run_silent() {
+function run_silent {
     local cmd="$*"
     [ -z "$cmd" ] && return 1
     echo " -> $cmd" 1>&2
     eval ${cmd} 1>/dev/null 2>/dev/null
 }
 
-function run_to_stdout() {
+function run_to_stdout {
     local cmd="$*"
     [ -z "$cmd" ] && return 1
     eval ${cmd} 2>&1
 }
 
-function run_hide() {
+function run_hide {
     local cmd="$*"
     [ -z "$cmd" ] && return 1
     eval ${cmd} 1>/dev/null 2>/dev/null
 }
 
-
-function last_modified() {
+function last_modified {
     local args="$*"
     [ -x "$1" ] && local args="`fs_realpath $1` ${@:2}"
     local cmd="find $args -printf \"%T@ %p\n\" | sort -n | tail -n 1"
+    eval ${cmd}
+}
+
+function last_modified_directory {
+    local args="$*"
+    [ -x "$1" ] && local args="`fs_realpath $1` ${@:2}"
+    local cmd="find $args -type d -not -path '*/.git*' -printf \"%T@ %p\n\" | sort -n | tail -n 1 | grep -Po '\d+' | head -n 1"
     eval ${cmd}
 }
