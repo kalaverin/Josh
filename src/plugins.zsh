9cee89240fc9beeffa1fe0b54f8d4aae5cdf892f
Alex Kalaverin <alex@kalaver.in>
2022-06-02 02:18:41 +0300

    develop: verbose plugins exceptions

--- src/plugins.zsh
+++ src/plugins.zsh
@@ -3,5 +3,10 @@ function plugins_autoload {
     do
         local loader="`fs_realpath $loader`"
         source "$loader"
+        local retval="$?"
+
+        if [ "$retval" -gt 0 ]; then
+            printf " ** fail ($0): something went wrong with '`fs_dirname $loader`', code: $retval\n" >&2
+        fi
     done
 }
