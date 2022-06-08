65f857d04d3915bf765c615d1eb5670fb9dc84bb
Alex Kalaverin <alex@kalaver.in>
2022-06-08 23:41:22 +0300

    develop: another logging

--- run/units/compat.sh
+++ run/units/compat.sh
@@ -25,7 +25,7 @@ function compat.executables {
         fi
     done
     if [ -n "$missing" ]; then
-        printf " ** fail ($0): missing required packages: $missing\n" >&2
+        fail $0 "missing required packages: $missing"
         return 1
     fi
 }
@@ -51,24 +51,24 @@ function compat.libraries {
                     local missing="$missing $lib"
                     continue
                 fi
-                printf " ++ warn ($0): pkg-config $lib failed, we try to which openssl.pc in $lookup_path\n" >&2
+                warn $0 "pkg-config $lib failed, we try to which openssl.pc in $lookup_path"
 
                 local openssl_path="$(fs_dirname `find "$lookup_path" -type f -name "openssl.pc" -follow 2>/dev/null | head -n 1`)"
                 if [ ! -d "$openssl_path" ]; then
-                    printf " ++ warn ($0): pkg-config $lib: nothing about openssl.pc in $lookup_path\n" >&2
+                    warn $0 "pkg-config $lib: nothing about openssl.pc in $lookup_path"
                     local missing="$missing $lib"
                     continue
                 fi
-                printf " ++ warn ($0): retry pkg-config $lib: openssl.pc found in $openssl_path\n" >&2
+                warn $0 "retry pkg-config $lib: openssl.pc found in $openssl_path"
 
                 export PKG_CONFIG_PATH="$openssl_path"
                 pkg-config --libs --cflags "$lib" 1>/dev/null 2>/dev/null
                 if [ "$?" -gt 0 ]; then
-                    printf " ++ warn ($0): pkg-config $lib: nothing about openssl.pc in $PKG_CONFIG_PATH\n" >&2
+                    warn $0 "pkg-config $lib: nothing about openssl.pc in $PKG_CONFIG_PATH"
                     local missing="$missing $lib"
                     continue
                 fi
-                printf " ++ warn ($0): pkg-config $lib: all ok, continue\n" >&2
+                warn $0 "pkg-config $lib: all ok, continue"
 
             else
                 local missing="$missing $lib"
@@ -77,7 +77,7 @@ function compat.libraries {
     done
 
     if [ -n "$missing" ]; then
-        printf " ** fail ($0): missing required libraries: $missing\n" >&2
+        fail $0 "missing required libraries: $missing"
         return 1
     fi
     return 0
@@ -94,8 +94,9 @@ function version_not_compatible {
 
 
 function compat.compliance {
-    if [ -n "$(uname | grep -i freebsd)" ]; then
-        printf " -- info ($0): os freebsd $(uname -srv)\n" >&2
+    local osname="$(uname -srv)"
+    if [ -n "$(echo "$osname" | grep -i freebsd)" ]; then
+        info $0 "os freebsd: $osname"
         export JOSH_OS="BSD"
 
         local cmd="sudo pkg install -y"
@@ -113,12 +114,12 @@ function compat.compliance {
             /usr/local/bin/gtar
         )
 
-    elif [ -n "$(uname | grep -i darwin)" ]; then
-        printf " -- info ($0): os macos $(uname -srv)\n" >&2
+    elif [ -n "$(echo "$osname" | grep -i darwin)" ]; then
+        info $0 "os macos: $osname"
         export JOSH_OS="MAC"
 
         if [ ! -x "$(builtin which -p brew 2>/dev/null)" ]; then
-            printf " -- info ($0): brew for MacOS strictly required, just run: curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | zsh\n" >&2
+            info $0 "brew for MacOS strictly required, just run: curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | zsh"
         fi
 
         local cmd="brew update && brew install"
@@ -137,7 +138,7 @@ function compat.compliance {
         )
 
 
-    elif [ -n "$(uname -srv | grep -i linux)" ]; then
+    elif [ -n "$(echo "$osname" | grep -i linux)" ]; then
         export JOSH_OS="LINUX"
         REQ_SYS_LIBS=(
             python
@@ -145,7 +146,7 @@ function compat.compliance {
 
         if [ -f "/etc/debian_version" ] || [ -n "$(uname -v | grep -Pi '(debian|ubuntu)')" ]; then
             REQ_SYS_BINS=( apt )
-            printf " -- info ($0): os debian-based $(uname -srv)\n" >&2
+            info $0 "os debian-based: $osname"
             [ -x "$(builtin which -p apt 2>/dev/null)" ] && local bin="apt" || local bin="apt-get"
 
             local cmd="(sudo $bin update --yes --quiet || true) && sudo $bin install --yes --quiet --no-remove"
@@ -154,22 +155,22 @@ function compat.compliance {
 
         elif [ -f "/etc/arch-release" ] || [ -n "$(uname -v | grep -Pi '(arch|manjaro)')" ]; then
             REQ_SYS_BINS=( pacman )
-            printf " -- info ($0): os arch: $(uname -srv)\n" >&2
+            info $0 "os arch: $osname"
 
             local cmd="sudo pacman --sync --noconfirm"
             local pkg="base-devel clang gcc git libevent openssl pkg-config postgresql-libs python3 tmux zsh"
 
-        elif [ -n "$(uname -srv | grep -i gentoo)" ]; then
-            printf " -- info ($0): os gentoo: $(uname -srv)\n" >&2
+        elif [ -n "$(echo "$osname" | grep -i gentoo)" ]; then
+            info $0 "os gentoo: $osname"
 
-        elif [ -n "$(uname -srv | grep -i microsoft)" ]; then
-            printf " -- info ($0): os unknown WSL: $(uname -srv)\n" >&2
+        elif [ -n "$(echo "$osname" | grep -i microsoft)" ]; then
+            info $0 "os unknown WSL: $osname"
 
         else
-            printf " -- info ($0): os unknown linux: $(uname -srv)\n" >&2
+            info $0 "os unknown linux: $osname"
         fi
     else
-        printf " -- info ($0): os unknown: $(uname -srv)\n" >&2
+        info $0 "os unknown: $osname"
         export JOSH_OS="UNKNOWN"
     fi
 
@@ -180,13 +181,12 @@ function compat.compliance {
         local msg="please, install required packages and try again"
         [ "$cmd" ] && local msg="$msg: $cmd $pkg"
 
-        if [ ! "$JOSH_SKIP_REQUIREMENTS_CHECK" ]; then
-            printf " ++ warn ($0): $msg\n" >&2 && return 1
-        else
-            printf " ++ warn ($0): $msg\n" >&2
+        warn $0 "$msg"
+        if [ -z "$JOSH_SKIP_REQUIREMENTS_CHECK" ]; then
+            return 1
         fi
     else
-        printf " -- info ($0): all requirements resolved, executives: $REQ_BINS $REQ_SYS_BINS, libraries: $REQ_LIBS $REQ_SYS_LIBS\n" >&2
+        info $0 "all requirements resolved, executives: $REQ_BINS $REQ_SYS_BINS, libraries: $REQ_LIBS $REQ_SYS_LIBS"
     fi
     return 0
 }
