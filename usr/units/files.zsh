7c8599a9838780d08fc800cdcab36c9366b1c6a2
Alex Kalaverin <alex@kalaver.in>
2022-06-21 09:35:57 +0300

    develop: bak & kab fixes

--- usr/units/files.zsh
+++ usr/units/files.zsh
@@ -1,29 +1,34 @@
 function __stream_pak {
-    if [ "$JOSH_OS" = 'BSD' ]; then
-        echo 'tar -cO --exclude-vcs --numeric-owner --sparse . -f -'
-    else
-        echo 'tar -cO --exclude-vcs --numeric-owner --sparse .'
+    if [ -z "$*" ]; then
+        fail $0 "args doesn't exist" >&2
+        return 1
     fi
+
+    local result="tar -cO $1 --numeric-owner --sparse ."
+    if [ "$JOSH_OS" = 'BSD' ]; then
+        local result="$result -f -"
+    fi
+    echo "$result"
 }
 
 
 function __stream_unpak {
+    local result='tar -x --numeric-owner --preserve-permissions'
     if [ "$JOSH_OS" = 'BSD' ]; then
-        echo 'tar -x --numeric-owner --preserve-permissions -f -'
-    else
-        echo 'tar -x --numeric-owner --preserve-permissions'
+        local result="$result -f -"
     fi
+    echo "$result"
 }
 
 
 function __setup.cfg.backup_file_get {
     local backup="$BAK_RESTORE"
     if [ "$backup" = "" ]; then
-        echo " - $0 fatal: BAK_RESTORE isn't set" 1>&2
+        fail $0 "BAK_RESTORE isn't set" >&2
         return 1
 
     elif [ -z "$JOSH_PAQ" ] || [ -z "$JOSH_QAP" ]; then
-        echo " - $0 fatal: zstd, xz, lz4 and gzip doesn't exists" 1>&2
+        fail $0 "zstd, xz, lz4 and gzip doesn't exists" >&2
         return 2
     fi
     echo "$backup"
@@ -32,66 +37,66 @@ function __setup.cfg.backup_file_get {
 
 function bak {
     if [ -f "$BAK_RESTORE" ]; then
-        echo " * backup already found: $BAK_RESTORE"
+        warn $0 "already found: $BAK_RESTORE"
 
     elif [ -z "$JOSH_PAQ" ] || [ -z "$JOSH_QAP" ]; then
-        echo " - fatal: zstd, xz, lz4 and gzip doesn't exists" 1>&2
-        return 1
+        fail $0 "zstd, xz, lz4 and gzip doesn't exists" >&2
+        return 127
     fi
 
-    local root="`git.this.root`"
+    local root="$(git.this.root)"
     [ -z "$root" ] && local root="$PWD"
 
-    local source="`fs.basename "$root"`"
+    local source="$(fs.basename "$root")"
     [ "$?" -gt 0 ] && return 1
 
-    local timemark="`date "+%Y.%m.%d-%H.%M.%S"`"
+    local timemark="$(date "+%Y.%m.%d-%H.%M.%S")"
     [ "$?" -gt 0 ] && return 2
 
-    local target="`temp.dir`/bak/$source"
+    local target="$(temp.dir)/bak/$source"
     [ "$?" -gt 0 ] && return 3
 
     local backup="$target/$timemark-$(get.name).tar"
     [ "$?" -gt 0 ] && return 4
 
-    run_show "mkdir -p \"$target\" 2>/dev/null; $(__stream_pak) | $JOSH_PAQ > $backup"
-    echo " => cat $backup | $JOSH_QAP | $(__stream_unpak)"
+    run_show "mkdir -p \"$target\" 2>/dev/null; $(__stream_pak --exclude-vcs) | $JOSH_PAQ > $backup"
+    info $0 "cat $backup | $JOSH_QAP | $(__stream_unpak)"
     export BAK_RESTORE="$backup"
 }
 
 
 function bakf {
     if [ -f "$BAK_RESTORE" ]; then
-        echo " * backup already found: $BAK_RESTORE"
+        warn $0 "already found: $BAK_RESTORE"
 
     elif [ -z "$JOSH_PAQ" ] || [ -z "$JOSH_QAP" ]; then
-        echo " - fatal: zstd, xz, lz4 and gzip doesn't exists" 1>&2
-        return 1
+        fail $0 " - fatal: zstd, xz, lz4 and gzip doesn't exists" >&2
+        return 127
     fi
 
-    local root="`git.this.root`"
+    local root="$(git.this.root)"
     [ -z "$root" ] && local root="$PWD"
 
-    local source="`fs.basename "$root"`"
+    local source="$(fs.basename "$root")"
     [ "$?" -gt 0 ] && return 1
 
-    local timemark="`date "+%Y.%m.%d-%H.%M.%S"`"
+    local timemark="$(date "+%Y.%m.%d-%H.%M.%S")"
     [ "$?" -gt 0 ] && return 2
 
-    local target="`temp.dir`/bak/$source"
+    local target="$(temp.dir)/bak/$source"
     [ "$?" -gt 0 ] && return 3
 
     local backup="$target/$timemark-$(get.name).tar"
     [ "$?" -gt 0 ] && return 4
 
-    run_show "mkdir -p \"$target\" 2>/dev/null; tar $(__stream_pak) | $JOSH_PAQ > $backup"
-    echo " => cat $backup | $JOSH_QAP | $(__stream_unpak)"
+    run_show "mkdir -p \"$target\" 2>/dev/null; $(__stream_pak --exclude-vcs-ignores) | $JOSH_PAQ > $backup"
+    info $0 "cat $backup | $JOSH_QAP | $(__stream_unpak)"
     export BAK_RESTORE="$backup"
 }
 
 
 function kab {
-    local backup="`__setup.cfg.backup_file_get 2>/dev/null`"
+    local backup="$(__setup.cfg.backup_file_get 2>/dev/null)"
     [ -z "$backup" ] && return "$?"
 
     git.is_clean || return "$?"
@@ -101,7 +106,7 @@ function kab {
 }
 
 function kabf {
-    local backup="`__setup.cfg.backup_file_get 2>/dev/null`"
+    local backup="$(__setup.cfg.backup_file_get 2>/dev/null)"
     [ -z "$backup" ] && return "$?"
 
     run_show "cat $backup | $JOSH_QAP | $(__stream_unpak)" && return 0
@@ -109,7 +114,7 @@ function kabf {
 }
 
 function bakrm {
-    local backup="`__setup.cfg.backup_file_get 2>/dev/null`"
+    local backup="$(__setup.cfg.backup_file_get 2>/dev/null)"
     [ -z "$backup" ] && return "$?"
 
     if [ -x "$(which rip)" ]; then
