68656db519421dc56ce65599b5ad7c9111a95f4d
Alex Kalaverin <alex@kalaver.in>
2022-07-11 14:16:16 +0300

    develop: fix configs

--- run/boot.sh
+++ run/boot.sh
@@ -1,3 +1,7 @@
 source "$(dirname "$0")/init.sh"
 depr "$0" "update ~/.zshrc, boot.sh deprecated"
-ASH_FORCE_CONFIGS=1 cfg.copy "$ASH/.zshrc" "$HOME/.zshrc"
+
+if [ -d "$ASH" ]; then
+    source "$ASH/run/units/configs.sh"
+    ASH_FORCE_CONFIGS=1 cfg.copy "$ASH/.zshrc" "$HOME/.zshrc"
+fi
