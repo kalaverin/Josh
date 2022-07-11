22bb24d11d128d5ee1d85495323dadf865de428d
Alex Kalaverin <alex@kalaver.in>
2022-07-11 15:17:32 +0300

    develop: check for ash

--- run/boot.sh
+++ run/boot.sh
@@ -3,7 +3,11 @@ depr "$0" "update ~/.zshrc, boot.sh deprecated"
 
 if [[ "$ASH" =~ '/.josh/custom/plugins/josh$' ]]; then
     fail "$0" "running Ash in old Josh context, force redeploy"
-    git clone --branch develop --single-branch 'https://github.com/kalaverin/Josh.git' "$HOME/.ash" && \
+
+    if [ ! -d "$HOME/.ash" ]; then
+        git clone --branch develop --single-branch 'https://github.com/kalaverin/Josh.git' "$HOME/.ash"
+    fi
+
     INSTALL=1 zsh "$HOME/.ash/run/init.sh"
 
 elif [ -d "$ASH" ]; then
