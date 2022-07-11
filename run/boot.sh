6353877259feba5da6fe69dfec049c075baea44c
Alex Kalaverin <alex@kalaver.in>
2022-07-11 15:34:29 +0300

    develop: do not clone again, just move josh to ash

--- run/boot.sh
+++ run/boot.sh
@@ -3,14 +3,15 @@ source "$(dirname "$0")/init.sh"
 if [[ "$ASH" =~ '/.josh/custom/plugins/josh$' ]]; then
     fail "$0" "running Ash in old Josh context, force redeploy"
 
-    if [ ! -d "$HOME/.ash" ]; then
-        git clone --branch develop --single-branch 'https://github.com/kalaverin/Josh.git' "$HOME/.ash"
+    if [ ! -d "$HOME/.ash" ] && [ -d "$HOME/.josh/custom/plugins/josh" ]; then
+        mv "$HOME/.josh/custom/plugins/josh" "$HOME/.ash"
+        ln -s "$HOME/.ash" "$HOME/.josh/custom/plugins/josh"
     fi
-
     INSTALL=1 zsh "$HOME/.ash/run/init.sh"
 
 elif [ -d "$ASH" ]; then
     depr "$0" "update ~/.zshrc, boot.sh deprecated"
+
     source "$ASH/run/units/configs.sh"
     ASH_FORCE_CONFIGS=1 cfg.copy "$ASH/.zshrc" "$HOME/.zshrc"
 fi
